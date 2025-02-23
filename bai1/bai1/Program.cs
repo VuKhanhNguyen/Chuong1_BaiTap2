bcf9772ce1b331f2583e5dcb529ba8d33a802938
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-02-23 19:03:55 +0700

    them method va them main

--- bai1/bai1/Program.cs
+++ bai1/bai1/Program.cs
@@ -11,13 +11,22 @@ namespace bai1
     {
        
 
-        public void OptionalParameter(string s1_118 = "Nguyễn Vũ Khanh")
+        public void OptionalParameter(string s1_118 = "Antony")
         {
-            Console.WriteLine("{0} là G.O.A.T của làng túc cầu", s1_118);
+            Console.WriteLine("{0} la G.O.A.T cua lang tuc cau", s1_118);
+        }
+        public void OptionalParameter2(int a, int b, int c = 2)
+        {
+            Console.WriteLine("{0}+{1}+{2}={3}", a, b, c, a + b + c);
         }
 
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.OptionalParameter("Messi");
+            //OptionalParameter();
+            p.OptionalParameter();
+
         }
     }
 }
