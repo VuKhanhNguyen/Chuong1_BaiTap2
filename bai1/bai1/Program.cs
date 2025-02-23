26b158c54a9259158726b470a5da34e26fe3e1ea
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-02-23 18:09:48 +0700

    them enumerate

--- bai1/bai1/Program.cs
+++ bai1/bai1/Program.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace bai1
 {
+    //Làm một bài tập có sử dụng optional parameter
     internal class Program
     {
+        enum FootballPlayer{
+            Antony,
+            Lewandowski,
+            Ronaldo,
+            Messi,
+            Haaland,
+            Mbappe,
+            Sane
+        }
+
         static void Main(string[] args)
         {
         }
