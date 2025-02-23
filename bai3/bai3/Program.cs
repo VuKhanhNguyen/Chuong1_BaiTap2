9afe8b8ca17798f4d51282938dae6e7d5f09f387
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-02-23 21:36:15 +0700

    them enumerate FootballMatchProgress

--- bai3/bai3/Program.cs
+++ bai3/bai3/Program.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace bai3
 {
+    //Làm một bài tập có định nghĩa lớp Enum, phương thức có dùng Optional parameter, kết hợp cả hai trong bài tập.
     internal class Program
     {
+        enum FootballMatchProgress
+        {
+            InMatch,
+            HalfTime,
+            FullTime,
+            Canceled,
+        }
         static void Main(string[] args)
         {
         }
