3987b4e172d05fb79b06f2d7f682d5b9ee24a922
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-02-23 22:55:19 +0700

    sua ten bien o method BayernPlayer, sua method BayernSplitCharacter, them main

--- bai4/bai4/Program.cs
+++ bai4/bai4/Program.cs
@@ -11,23 +11,32 @@ namespace bai4
         //Làm một bài tập sử dụng array, sử dụng foreach
         static void BayernPlayer(string[] arr_118)
         {
+            int index_118 = 1;
             foreach (string a_118 in arr_118)
             {
-                Console.WriteLine(a_118);
+                Console.WriteLine("{0}. {1}",index_118, a_118);
+                index_118++;
             }
         }
 
         static void BayernSplitCharacters(char[] arr2_118)
         {
+            int index_118 = 1;
             foreach (char a2_118 in arr2_118)
             {
-                Console.WriteLine(a2_118);
+
+                Console.WriteLine("{0}. {1}",index_118,a2_118);
+                index_118++;
             }
         }
 
         static void Main(string[] args)
         {
+            string[] arr_118 = { "Kane", "Olise", "Musiala", "Muller", "Sane", "Gnabry", "Coman" };
+            BayernPlayer(arr_118);
 
+            char[] arr2_118 = "Bayern".ToCharArray();
+            BayernSplitCharacters(arr2_118);
         }
     }
 }
