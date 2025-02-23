8ebd44de605e537d0a2deeb2fe65b247b4977b1c
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-02-23 20:15:12 +0700

    them enum FootballClub

--- bai2/bai2/Program.cs
+++ bai2/bai2/Program.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace bai2
 {
+    //Làm một bài tập sử dụng Enum, có sử dụng các phương thức hỗ trợ nó để in ra màn hình
     internal class Program
     {
+        enum FootballClub
+        {
+            Bayern,
+            Leverkusen,
+            Frankfurt,
+            Freiburg,
+            Mainz05,
+            Leipzig
+        }
+
         static void Main(string[] args)
         {
         }
