8c0273f2d49ee84137c6cfe99d34d4c5aee372ac
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-02-24 19:06:14 +0700

    them method PointStatus

--- bai5/bai5/Point.cs
+++ bai5/bai5/Point.cs
@@ -6,20 +6,32 @@ using System.Threading.Tasks;
 
 namespace bai5
 {
-    internal class Point
-    {
-        enum PointColor_118
+
+enum PointColor_118
         {
             LightBlue,
             BloodRed,
             Gold
         }
 
+    internal class Point
+    {
+        
         private int x_118 { get; set; }
         private int y_118 { get; set; }
 
-        PointColor_118 pc { get; set; }
+        PointColor_118 pc_118 { get; set; }
 
-       
+        public Point(int x_118, int y_118, PointColor_118 pc_118)
+        {
+            this.x_118 = x_118;
+            this.y_118 = y_118;
+            this.pc_118 = pc_118;
+        }
+
+        public void PointStatus()
+        {
+            Console.WriteLine("Point co toa do ({0},{1}) va co mau {3}", this.x_118, this.y_118, this.pc_118);
+        }
     }
 }
