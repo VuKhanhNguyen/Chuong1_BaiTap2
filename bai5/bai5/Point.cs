629ada1512efb1ed68d4dbcd422ceebc2720fc64
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-02-24 18:40:19 +0700

    them enum, khai bao bien

--- bai5/bai5/Point.cs
+++ bai5/bai5/Point.cs
@@ -8,5 +8,18 @@ namespace bai5
 {
     internal class Point
     {
+        enum PointColor_118
+        {
+            LightBlue,
+            BloodRed,
+            Gold
+        }
+
+        private int x_118 { get; set; }
+        private int y_118 { get; set; }
+
+        PointColor_118 pc { get; set; }
+
+       
     }
 }
